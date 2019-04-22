efda9d1e1cf06c54b82d629923fb0bd16406e627
Tonmoy <tonmoy24@gmail.com>
2019-04-22 15:34:52 +1200

    Add user model

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace dotnet_webapi_jwt_auth.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) {}
 
+        public DbSet<User> Users { get; set; }
     }
 
 }
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace dotnet_webapi_jwt_auth.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
