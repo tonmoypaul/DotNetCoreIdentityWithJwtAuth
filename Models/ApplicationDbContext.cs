957006378c8fa8be32acaabdf2d7a36e5a7af352
Tonmoy <tonmoy24@gmail.com>
2019-04-22 15:17:53 +1200

    Added watcher and mysql nuget pkgs, connection string, db context, mysql middleware

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_webapi_jwt_auth.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {}
+
+    }
+
+}
